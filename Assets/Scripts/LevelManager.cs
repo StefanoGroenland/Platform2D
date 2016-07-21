49280e484b612bde3b35cef7cf528077fb44d6a5
Stefano <stefano.groenland@gmail.com>
2016-07-22 01:04:08 +0200

    base files

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+    private PlayerController player;
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+    public float respawnDelay;
+    public int pointPenaltyOnDeath;
+    
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RespawnPlayer(){
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        ScoreManager.AddPoints(-pointPenaltyOnDeath);
+        Debug.Log("Player respawn");
+        yield return new WaitForSeconds(respawnDelay);
+        player.transform.position = currentCheckpoint.transform.position;
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private float moveVelocity;
+    public float jumpHeight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    private bool doubleJumped;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start (){
+        anim = GetComponent<Animator>();
+	}
+
+    void FixedUpdate (){
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+	
+	// Update is called once per frame
+	void Update (){
+
+        if (grounded)
+        {
+            doubleJumped = false;
+        }
+
+        anim.SetBool("Grounded", grounded);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !doubleJumped && !grounded)
+        {
+            Jump();
+            doubleJumped = true;
+        }
+
+        moveVelocity = 0f;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveVelocity = moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveVelocity = -moveSpeed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+        if(GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+    }
+
+}
